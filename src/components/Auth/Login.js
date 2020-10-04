31473eaad1562ffa375cf584abbb28ab15382a01
nati_abebaw <natiabebaw35@gmail.com>
2020-10-04 15:43:32 -0300

    resusable hook in Login.js

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,35 +1,60 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import UseFormValidation from './useFormValidation'
+
+const INITIAL_STATE = {
+	name: '',
+	email: '',
+	password: '',
+};
 
 const Login = () => {
 
-    const [login, setLogin] = useState(true);
+    const { handleSubmit, handleChange, values } = UseFormValidation(INITIAL_STATE);
+	const [login, setLogin] = useState(true);
 
 	return (
 		<div>
-			<h2 className="mv3">{login ? "Login":"Create Account"}</h2>
+			<h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
 
-			<form className="flex flex-column">
-                {!login && (<input 
-                    type="text" 
-                    placeholder="Your name" 
-                    autoComplete="off" />
-                )}
-                <input 
-                    type="email" 
-                    placeholder="Your email" 
-                    autoComplete="off" />
-                <input 
-                    type="email" 
-                    placeholder={login ? "Your password":"Choose a secure password "} />
+			<form onSubmit={handleSubmit} className="flex flex-column">
+				{!login && (
+					<input
+						onChange={handleChange}
+						type="text"
+						name="name"
+						value={values.name}
+						placeholder="Your name"
+						autoComplete="off"
+					/>
+				)}
+				<input
+					onChange={handleChange}
+					type="email"
+					name="email"
+					value={values.email}
+					placeholder="Your email"
+					autoComplete="off"
+				/>
+				<input
+					onChange={handleChange}
+					type="password"
+					name="password"
+					value={values.password}
+					placeholder={login ? 'Your password' : 'Choose a secure password '}
+				/>
 
-                <div>
-                    <button type="submit" className="button pointer mr2">Submit</button>
-                    <button 
-                        type="button" 
-                        className="button pointer"
-                        onClick={() => setLogin(prev => !prev)}
-                        >{login ? "need to create an account?":"already have an account?"}</button>
-                </div>
+				<div>
+					<button type="submit" className="button pointer mr2">
+						Submit
+					</button>
+					<button
+						type="button"
+						className="button pointer"
+						onClick={() => setLogin((prev) => !prev)}
+					>
+						{login ? 'need to create an account?' : 'already have an account?'}
+					</button>
+				</div>
 			</form>
 		</div>
 	);
---
+++ src/components/Auth/useFormValidation.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function useFormValidation(initialState)  {
+    
+        const [values, setValues] = React.useState(initialState)
+
+        const handleChange = (event) => {
+            event.persist();
+            setValues(previousValues => ({
+                ...previousValues,
+                [event.target.name]: event.target.value
+            }));
+        }
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            console.log(values)
+        }
+            
+        //return to Login Component
+        return { handleSubmit, handleChange, values }
+}
+
+
+export default useFormValidation;
