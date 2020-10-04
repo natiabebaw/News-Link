741fc6f3d6194dc5c07fece2a08ce11cefe002b3
nati_abebaw <natiabebaw35@gmail.com>
2020-10-04 14:20:55 -0300

    Header.js implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { withRouter, NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
-export default Header;
+	return (
+		<div className="header">
+			<div className="flex">
+				<img src="/logo.png" alt="logo" className="logo" />
+				<NavLink to="/" className="header-title">
+					Hook News
+				</NavLink>
+				<NavLink to="/" className="header-link">
+					new
+				</NavLink>
+
+				<span className="divider">|</span>
+				<NavLink to="/top" className="header-link">
+					top
+				</NavLink>
+                <span className="divider">|</span>
+                <NavLink to="/search" className="header-link">
+					Search
+				</NavLink>
+				<span className="divider">|</span>
+				<NavLink to="/create" className="header-link">
+					submit
+				</NavLink>
+			</div>
+
+			<div className="flex">
+				<NavLink to="/login" className="header-link">
+					login
+				</NavLink>
+			</div>
+		</div>
+	);
+};
+export default withRouter(Header);
